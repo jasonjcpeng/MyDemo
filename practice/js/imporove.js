57ab5fe92bcec087e492f085bdac23cedd5c6378
JasonJCpeng <JasonJCpeng@hotmail.com>
2016-06-14 20:37:39 +0800

    增加成员变量 以及 闭包的学习Demo

--- practice/js/imporove.js
+++ practice/js/imporove.js
@@ -3,17 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- //--------------------------------------
-            var foo = 1;
-            function bar(){
-                if(!foo){
-                    var foo = 10;
-                }
-                console.log(foo);
-            }
-            bar();
-            console.log(foo); 
-            //--------------------------以下为实际执行代码，变量提升后编译器执行...
+(function () {
+    //--------------------------------------
+    var foo = 1;
+    function bar() {
+        if (!foo) {
+            var foo = 10;
+        }
+        console.log(foo);
+    }
+    bar();
+    console.log(foo);
+    //--------------------------以下为实际执行代码，变量提升后编译器执行...
 //            var foo;
 //              foo = 1;
 //            function bar(){
@@ -23,25 +24,29 @@
 //                console.log(foo); //foo = 10;
 //            }
 //          
-            
-            //-------------------例二--------------------
-            var a = 2;
-            function b(){
-                a=10;
-                return ;
-                function a(){
-                }
-            }
-            b();
-            console.log(a);
-            //---------------------------提升后----------------
-            var a;
-             a=1;
-            function b(){
-                function a(){};
-                a=10;
-                return ;
-            }
-            console.log(a);//a = 1; b()中的a 只是一个内部变量
-            //-------------------------------------------
+
+    //-------------------例二--------------------
+    var a = 2;
+    function b() {
+        a = 10;
+        return;
+        function a() {
+        }
+    }
+    b();
+    console.log(a);
+    //---------------------------提升后----------------
+    var a;
+    a = 1;
+    function b() {
+        function a() {
+        }
+        ;
+        a = 10;
+        return;
+    }
+    console.log(a);//a = 1; b()中的a 只是一个内部变量
+    //-------------------------------------------   
+})();
+
 
--- practice/js/prototypeAndConstructor.js
+++ practice/js/prototypeAndConstructor.js
@@ -3,83 +3,86 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-  function foo(){
-             this.value = 11;
-         }
-         foo.prototype ={
-           method:function(){
-               console.log("From foo.prototype.method()");
-           }  
-         };
-         
-         function bar(){
-             this.barv = 123;
-         }
-         
-         bar.prototype = new foo();//将foo的原型赋给bar；至于为什么不是 bar.prototype = foo.prototype  因为一旦bar的原型进行修改也会破坏foo的原型
-         //当重新定义了 foo.prototype 时 等同于做了如下操作
-         //foo.prototype = new Object({
-         //   method:function(){
-         //     console.log("From foo.prototype.method()");
-         //   }  
-         // });
-         //于是foo.prototyle.constructor === Object     故而需要重新覆盖constructor
-         bar.prototype.foo = "Hello world";
-         bar.prototype.constructor = bar;
-         var b = new bar();
-         console.log(b);
-         //----------------------------call继承---------查找是否有非原型属性
-         function person(name,age){
-             this.age = age;
-             this.init.call(this,name);
-             this.sayName = function(){
-                 console.log("Name is"+this.name);
-             };
-         }
-         
-         person.prototype={
-             init:function (name){
-               this.name = name;  
-             },
-             sayHi:function(msg){
-                 console.log("Say"+msg);
-             }
-         };
-         person.prototype.constructor = person;
-         var ming = new person('小明');
-         console.log(ming);
-         ming.sayName();
-        (function(){ //加入新作用域；n 将不能在外部调用
-           for(var n in ming){
-             ming.hasOwnProperty(n)&& console.log(n)+"1"||console.log(n);
-            }
-         })();
-        //-------------------------------上述代码内部变量私有化
-        function newperson(name,age){
-             this.age = age;
-             this.init.call(this,name);
-             this.sayName = function(){
-                 console.log(this);
-                 console.log("Name is"+this.name);
-             };
-             return {name:this.name,age:this.age,sayName:this.sayName};
-         }
-         
-         newperson.prototype={
-             init:function (name){
-               this.name = name;  
-             },
-             sayHi:function(msg){
-                 console.log("Say"+msg);
-             }
-         };
-         newperson.prototype.constructor = newperson;
-         var ming = new newperson('小明',12);
-         console.log(ming);
-         ming.sayName();
-        (function(){ //加入新作用域；n 将不能在外部调用
-           for(var n in ming){
-             ming.hasOwnProperty(n)&& console.log(n)+"1"||console.log(n);
-            }
-         })();
+(function () {
+    function foo() {
+        this.value = 11;
+    }
+    foo.prototype = {
+        method: function () {
+            console.log("From foo.prototype.method()");
+        }
+    };
+
+    function bar() {
+        this.barv = 123;
+    }
+
+    bar.prototype = new foo();//将foo的原型赋给bar；至于为什么不是 bar.prototype = foo.prototype  因为一旦bar的原型进行修改也会破坏foo的原型
+    //当重新定义了 foo.prototype 时 等同于做了如下操作
+    //foo.prototype = new Object({
+    //   method:function(){
+    //     console.log("From foo.prototype.method()");
+    //   }  
+    // });
+    //于是foo.prototyle.constructor === Object     故而需要重新覆盖constructor
+    bar.prototype.foo = "Hello world";
+    bar.prototype.constructor = bar;
+    var b = new bar();
+    console.log(b);
+    //----------------------------call继承---------查找是否有非原型属性
+    function person(name, age) {
+        this.age = age;
+        this.init.call(this, name);
+        this.sayName = function () {
+            console.log("Name is" + this.name);
+        };
+    }
+
+    person.prototype = {
+        init: function (name) {
+            this.name = name;
+        },
+        sayHi: function (msg) {
+            console.log("Say" + msg);
+        }
+    };
+    person.prototype.constructor = person;
+    var ming = new person('小明');
+    console.log(ming);
+    ming.sayName();
+    (function () { //加入新作用域；n 将不能在外部调用
+        for (var n in ming) {
+            ming.hasOwnProperty(n) && console.log(n) + "1" || console.log(n);
+        }
+    })();
+    //-------------------------------上述代码内部变量私有化
+    function newperson(name, age) {
+        this.age = age;
+        this.init.call(this, name);
+        this.sayName = function () {
+            console.log(this);
+            console.log("Name is" + this.name);
+        };
+        return {name: this.name, age: this.age, sayName: this.sayName};
+    }
+
+    newperson.prototype = {
+        init: function (name) {
+            this.name = name;
+        },
+        sayHi: function (msg) {
+            console.log("Say" + msg);
+        }
+    };
+    newperson.prototype.constructor = newperson;
+    var ming = new newperson('小明', 12);
+    console.log(ming);
+    ming.sayName();
+    (function () { //加入新作用域；n 将不能在外部调用
+        for (var n in ming) { //如何通过JavaScript对象中的成员变量迭代
+            ming.hasOwnProperty(n) && console.log(n) + "1" || console.log(n);
+        }
+    })();
+})();
+
 
