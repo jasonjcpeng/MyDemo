239f373d0f985cd734685f1f0ba0c8c73e8f8aef
JasonJCpeng <JasonJCpeng@hotmail.com>
2016-06-21 16:55:57 +0800

    增加快速排序与冒泡排序

---
+++ practice/js/sort.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var sort = {
+    quickSort: function (arr) {
+        if (typeof (arr) === "object") {
+            if (arr.length <= 1) {
+                return arr;
+            }
+            var pivotIndex = Math.floor(arr.length / 2);
+            var pivot = arr.splice(pivotIndex, 1)[0];
+            var left = [];
+            var right = [];
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i] < pivot) {
+                    left.push(arr[i]);
+                } else {
+                    right.push(arr[i]);
+                }
+            }
+            return this.quickSort(left).concat([pivot], this.quickSort(right));
+        } else {
+            return false;
+        }
+    },
+    popSort: function (arr) {
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr.length - 1; j++) {
+                if (arr[i] < arr[j]) {
+                    var temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+};
