3e25873344d2b71a658624cb61d2f09df0b381a8
JasonJCpeng <JasonJCpeng@hotmail.com>
2016-07-20 09:22:25 +0800

    加入Angular.js库 以及部分H5的特性和页面模式

---
+++ Node/node.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wet on 2016/7/7.
+ */
+exports.say=function () {
+    var message = "lalalalala我是第一个";
+    return message;
+}
---
+++ Node/nodeTwo.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wet on 2016/7/7.
+ */
+var node = require("./node");
+var message = node.say();
+console.log("run");
+console.log(message);
